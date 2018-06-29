d63c2170cde1a1a92bd3435ad0bf209b606b5528
Oliver Beckstein <oliver.beckstein@asu.edu>
2018-06-29 11:47:47 -0700

    cron.sh from astropy/astropy-benchmarks
    - https://github.com/astropy/astropy-benchmarks/blob/master/cron.sh
    - used under the BSD-3-Clause license

---
+++ scripts/cron.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -xe
+# astropy/astropy-benchmarks cron.sh
+# License: BSD-3-Clause
+
+MACHINE=`python -c "from asv.machine import Machine; print(Machine.load('~/.asv-machine.json').machine)"`
+
+echo "asv: "`asv --version`
+echo "Machine: "$MACHINE
+
+git clean -fxd
+git checkout master
+git pull origin master
+
+# We now run all benchmarks since the last one run in this benchmarks repository. This assumes
+# you have previously run at least ``asv run HEAD^!`` and added the results to the repository
+# otherwise running ``asv run NEW`` will run all benchmarks since the start of the project.
+# We add || true to make sure that if no commits are run (because there aren't any) things don't
+# fail. But asv should have a way to return a zero status code in that case, so we should fix
+# that in future. The timeout command is used to make sure that the command finishes before
+# the next cron job - the timeout value is in seconds and can be adjusted.
+
+# On Linux - using taskset -c 0 ensures that the same core is always used when running the benchmarks.
+taskset -c 0 asv run NEW || true
+timeout 7200 taskset -c 0 asv run ALL --skip-existing-commits || true
+
+# On MacOSX:
+# asv run NEW || true
+# timeout 7200 asv run ALL --steps 10 --skip-existing-commits || true
+
+git add results/$MACHINE
+git commit -m "New results from $MACHINE"
+
+git push origin master
+asv gh-pages --no-push
+git push -f origin gh-pages
+
